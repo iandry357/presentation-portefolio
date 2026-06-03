4e6ec472847f3009dd0f43f7f2998c7cb71a6480
Iandry RAKOTONIAINA <iandry.rakoto7@gmail.com>
2026-06-03 13:15:44 +0200

    git action savencia

--- infra/main.tf
+++ infra/main.tf
@@ -14,6 +14,7 @@ terraform {
     skip_credentials_validation = true
     skip_region_validation      = true
     skip_requesting_account_id  = true
+    force_path_style            = true
   }
 }
 
